1ba4ee5a3cf627a3da35a1bbdc281742debfdfe8
Your Name <grossirm@hotmail.com>
2025-07-19 14:03:57 -0300

    added alloewd host railway

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = "django-insecure-k7w#=-%)5usygt2mu*2qgt^akq^9t0dv+)s-rpzyic#su9s_ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['pfd-production.up.railway.app', 'https//pfd-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['pfd-production.up.railway.app', 'https//pfd-production.up.railway.app']
 
 # Application definition
 
